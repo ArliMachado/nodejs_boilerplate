a179b516d7e094605417899d81e5931f6617bc13
Arli Machado <arli.machado@gmail.com>
2022-02-10 18:29:11 -0300

    Configurando husky

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (request: Request, response: Response) =>
-  response.json({ message: 'Hello World1' }),
-);
+app.get('/', (request: Request, response: Response) => {
+  // const teste = '';
+  return response.json({ message: 'Hello World1' });
+});
 
 app.listen(3333, () => console.log('Server is running!'));
